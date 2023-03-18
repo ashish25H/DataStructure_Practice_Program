08fce8c8e6892739b673ba0dc4d12ea2df4ea17d
Ashish <104784992+ashish25H@users.noreply.github.com>
2023-03-18 19:22:19 +0530

    Add files via upload

--- Data Structure/binarySearchTree.js
+++ Data Structure/binarySearchTree.js
@@ -40,6 +40,46 @@ class BinarySearchTree {
       this.insertVal(node, this.root);
     }
   }
+
+  search(root, value) {
+    if (this.isEmpty()) {
+      console.log(`Tree is Empty`);
+    }
+
+    if (!root) {
+      console.log(`Value is not Present`);
+    } else if (root.value === value) {
+      console.log(`value is Present`);
+    } else if (value < root.value) {
+      this.search(root.left, value);
+    } else if (value > root.value) {
+      this.search(root.right, value);
+    }
+  }
+
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
+
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
+
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -49,3 +89,8 @@ tree.insertNode(10);
 tree.insertNode(12);
 tree.insertNode(8);
 console.log(tree.isEmpty());
+tree.search(tree.root, 20);
+tree.preOrder(tree.root);
+tree.postOrder(tree.root);
+tree.inOrder(tree.root);
+
